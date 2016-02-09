326b324e9ee03ff1137df92cb80d3c066b5defec
Oleg Galaburda <burdiuz@gmail.com>
2016-02-10 01:07:52 +0200

    add proper example

---
+++ example/worker.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Oleg Galaburda on 10.02.16.
+ */
+importScripts(
+  '../bower_components/event-dispatcher/dist/event-dispatcher.js',
+  '../dist/worker-event-dispatcher.js'
+);
+var api = WorkerEventDispatcher.self();
+api.addEventListener('time:request', function() {
+  setTimeout(function() {
+    api.dispatchEvent('time:response', Date.now());
+  }, 200);
+});
